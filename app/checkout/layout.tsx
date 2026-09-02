8d42baf9659f720a42f2170e0aee99c1f93686e3
muhammadaldy198 <muhammadaldy198@gmail.com>
2026-09-02 23:40:37 +0800

    Aktifkan tab nominal pada checkout

--- app/checkout/layout.tsx
+++ app/checkout/layout.tsx
@@ -1,11 +1,13 @@
 import { CheckoutUiEnhancer } from "@/components/checkout-ui-enhancer";
 import { CheckoutSpacingTuning } from "@/components/checkout-spacing-tuning";
+import { CheckoutPackageTabs } from "@/components/checkout-package-tabs";
 
 export default function CheckoutLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <CheckoutUiEnhancer />
       <CheckoutSpacingTuning />
+      <CheckoutPackageTabs />
       {children}
     </>
   );
