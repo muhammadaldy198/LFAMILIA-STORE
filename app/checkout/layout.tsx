50a828ae31e0a5b0b6ae835467051642da1e3996
muhammadaldy198 <muhammadaldy198@gmail.com>
2026-09-03 04:17:44 +0800

    Aktifkan redirect otomatis Midtrans di checkout

--- app/checkout/layout.tsx
+++ app/checkout/layout.tsx
@@ -1,3 +1,4 @@
+import { CheckoutMidtransRedirect } from "@/components/checkout-midtrans-redirect";
 import { CheckoutUiEnhancer } from "@/components/checkout-ui-enhancer";
 import { CheckoutSpacingTuning } from "@/components/checkout-spacing-tuning";
 import { CheckoutPackageTabs } from "@/components/checkout-package-tabs";
@@ -10,6 +11,7 @@ export default function CheckoutLayout({ children }: { children: React.ReactNode
       <CheckoutSpacingTuning />
       <CheckoutPackageTabs />
       <CheckoutVoucherMode />
+      <CheckoutMidtransRedirect />
       {children}
     </>
   );
