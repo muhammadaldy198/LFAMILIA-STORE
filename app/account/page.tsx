e60cf4a903731b7f00094598d5965ff89a5f8441
muhammadaldy198 <muhammadaldy198@gmail.com>
2026-09-03 00:49:19 +0800

    Pasang ringkasan tier pada akun member

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,6 +1,14 @@
 import { CustomerAccount } from "@/components/customer-account";
+import { CustomerMembershipSummary } from "@/components/customer-membership-summary";
 import { StoreLayout } from "@/components/store-layout";
 
 export default function AccountPage() {
-  return <StoreLayout><main className="mx-auto min-h-[70vh] max-w-7xl px-4 py-10 sm:px-6 lg:px-8"><CustomerAccount /></main></StoreLayout>;
+  return (
+    <StoreLayout>
+      <main className="mx-auto min-h-[70vh] max-w-7xl px-4 py-10 sm:px-6 lg:px-8">
+        <CustomerMembershipSummary />
+        <CustomerAccount />
+      </main>
+    </StoreLayout>
+  );
 }
