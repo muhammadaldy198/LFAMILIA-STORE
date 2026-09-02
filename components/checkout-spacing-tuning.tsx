b90e813a7fa802cd97d4d6e574a154577f837d73
muhammadaldy198 <muhammadaldy198@gmail.com>
2026-09-03 00:06:35 +0800

    Naikkan posisi nama produk checkout sedikit

--- components/checkout-spacing-tuning.tsx
+++ components/checkout-spacing-tuning.tsx
@@ -19,7 +19,7 @@ export function CheckoutSpacingTuning() {
           }
 
           [data-lf-product-info="true"] {
-            padding-top: 2.2rem !important;
+            padding-top: 1.7rem !important;
           }
 
           [data-lf-product-features="true"] {
