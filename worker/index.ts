f980b23b6fa5cf347f2ae4d5f4d388220d5d954b
muhammadaldy198 <muhammadaldy198@gmail.com>
2026-09-08 05:23:37 +0800

    fix: recover promotion reservation leases safely

--- worker/index.ts
+++ worker/index.ts
@@ -3,7 +3,10 @@ import { handleImageOptimization, DEFAULT_DEVICE_SIZES, DEFAULT_IMAGE_SIZES } fr
 import handler from "vinext/server/app-router-entry";
 import { getPublicBaseUrl, setRuntimeEnv } from "../lib/server/runtime-env";
 import { hydrateIntegrationRuntimeEnv } from "../lib/server/integration-config";
-import { recoverStaleAutomaticOrders } from "../lib/server/orders";
+import {
+  recoverAbandonedPromotionReservations,
+  recoverStaleAutomaticOrders,
+} from "../lib/server/orders";
 import { syncDigiflazzPrices } from "../lib/server/digiflazz-pricing";
 import { cleanupSecurityRateLimits } from "../lib/server/security";
 import { verifyCloudflareAccess } from "../lib/server/cloudflare-access";
@@ -132,6 +135,7 @@ const worker = {
       Promise.resolve()
         .then(() => recoverStaleAutomaticOrders(getPublicBaseUrl()))
         .catch(() => undefined),
+      recoverAbandonedPromotionReservations().catch(() => undefined),
     ]));
   },
 };
