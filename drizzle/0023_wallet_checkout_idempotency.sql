e33f1fd6b607dc763c1b18b63eb94f8145e5dfe6
muhammadaldy198 <muhammadaldy198@gmail.com>
2026-09-08 07:34:11 +0800

    cleanup: remove 0023_wallet_checkout_idempotency.sql

--- drizzle/0023_wallet_checkout_idempotency.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `orders` ADD `wallet_checkout_key` text;
---> statement-breakpoint
-CREATE UNIQUE INDEX `orders_wallet_checkout_key_unique` ON `orders` (`customer_id`, `wallet_checkout_key`);
