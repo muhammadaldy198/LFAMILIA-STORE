c0adb9369caa9202852558a99f4c482b43a8dd73
muhammadaldy198 <muhammadaldy198@gmail.com>
2026-09-08 07:34:06 +0800

    cleanup: remove 0014_midtrans_gateway.sql

--- drizzle/0014_midtrans_gateway.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE wallet_settings ADD COLUMN midtrans_topup_enabled INTEGER DEFAULT 0 NOT NULL;
-ALTER TABLE wallet_settings ADD COLUMN midtrans_checkout_enabled INTEGER DEFAULT 0 NOT NULL;
-
-ALTER TABLE wallet_topups ADD COLUMN midtrans_transaction_id TEXT;
-ALTER TABLE wallet_topups ADD COLUMN midtrans_payment_url TEXT;
-
-ALTER TABLE orders ADD COLUMN midtrans_transaction_id TEXT;
-ALTER TABLE orders ADD COLUMN midtrans_payment_url TEXT;
