8060c4c33c0cdcfab124598dfbf4a7694a3e1a2e
muhammadaldy198 <muhammadaldy198@gmail.com>
2026-09-18 14:30:23 +0800

    fix: remount account form when auth mode changes

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -14,7 +14,7 @@ export default async function AccountPage({
     <StoreLayout>
       <main className="mx-auto min-h-[70vh] max-w-7xl px-4 py-10 sm:px-6 lg:px-8">
         <CustomerMembershipSummary />
-        <CustomerAccount initialMode={initialMode} />
+        <CustomerAccount key={initialMode} initialMode={initialMode} />
       </main>
     </StoreLayout>
   );
